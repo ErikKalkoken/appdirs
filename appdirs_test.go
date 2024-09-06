dac1dc5e98a2581593dea5ed5827cc21e4f6bf89
ErikKalkoken <erik.kalkoken@gmail.com>
2024-09-06 03:29:28 +0200

    Fix username error in windows tests

--- appdirs_test.go
+++ appdirs_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os/user"
 	"runtime"
+	"strings"
 	"testing"
 )
 
@@ -12,35 +13,49 @@ func getExpectations() map[string]string {
 	if err != nil {
 		panic(err)
 	}
-
+	var userName string
+	if runtime.GOOS == "windows" {
+		userName = extractDomain(u.Username)
+	} else {
+		userName = u.Username
+	}
 	expectations := map[string]map[string]string{
 		"darwin": {
-			"UserData":   fmt.Sprintf("/Users/%s/Library/Application Support/Testing/1.0.0", u.Username),
-			"UserCache":  fmt.Sprintf("/Users/%s/Library/Caches/Testing/1.0.0", u.Username),
-			"UserConfig": fmt.Sprintf("/Users/%s/Library/Application Support/Testing/1.0.0", u.Username),
-			"UserLog":    fmt.Sprintf("/Users/%s/Library/Logs/Testing/1.0.0", u.Username),
+			"UserData":   fmt.Sprintf("/Users/%s/Library/Application Support/Testing/1.0.0", userName),
+			"UserCache":  fmt.Sprintf("/Users/%s/Library/Caches/Testing/1.0.0", userName),
+			"UserConfig": fmt.Sprintf("/Users/%s/Library/Application Support/Testing/1.0.0", userName),
+			"UserLog":    fmt.Sprintf("/Users/%s/Library/Logs/Testing/1.0.0", userName),
 			"SiteData":   "/Library/Application Support/Testing/1.0.0",
 			"SiteConfig": "/Library/Application Support/Testing/1.0.0",
 		},
 		"linux": {
-			"UserData":   fmt.Sprintf("/home/%s/.local/share/Testing/1.0.0", u.Username),
-			"UserCache":  fmt.Sprintf("/home/%s/.cache/Testing/1.0.0", u.Username),
-			"UserConfig": fmt.Sprintf("/home/%s/.config/Testing/1.0.0", u.Username),
-			"UserLog":    fmt.Sprintf("/home/%s/.cache/Testing/1.0.0/log", u.Username),
+			"UserData":   fmt.Sprintf("/home/%s/.local/share/Testing/1.0.0", userName),
+			"UserCache":  fmt.Sprintf("/home/%s/.cache/Testing/1.0.0", userName),
+			"UserConfig": fmt.Sprintf("/home/%s/.config/Testing/1.0.0", userName),
+			"UserLog":    fmt.Sprintf("/home/%s/.cache/Testing/1.0.0/log", userName),
 			"SiteData":   "/usr/local/share/Testing/1.0.0",
 			"SiteConfig": "/etc/xdg/Testing/1.0.0",
 		},
 		"windows": {
-			"UserData":   fmt.Sprintf("C:\\Users\\%s\\AppData\\Local\\Tester\\Testing\\1.0.0", u.Username),
-			"UserCache":  fmt.Sprintf("C:\\Users\\%s\\AppData\\Local\\Tester\\Testing\\Cache\\1.0.0", u.Username),
-			"UserConfig": fmt.Sprintf("C:\\Users\\%s\\AppData\\Local\\Tester\\Testing\\1.0.0", u.Username),
-			"UserLog":    fmt.Sprintf("C:\\Users\\%s\\AppData\\Local\\Tester\\Testing\\1.0.0\\Logs", u.Username),
+			"UserData":   fmt.Sprintf("C:\\Users\\%s\\AppData\\Local\\Tester\\Testing\\1.0.0", userName),
+			"UserCache":  fmt.Sprintf("C:\\Users\\%s\\AppData\\Local\\Tester\\Testing\\Cache\\1.0.0", userName),
+			"UserConfig": fmt.Sprintf("C:\\Users\\%s\\AppData\\Local\\Tester\\Testing\\1.0.0", userName),
+			"UserLog":    fmt.Sprintf("C:\\Users\\%s\\AppData\\Local\\Tester\\Testing\\1.0.0\\Logs", userName),
 			"SiteData":   "C:\\ProgramData\\Tester\\Testing\\1.0.0",
 			"SiteConfig": "C:\\ProgramData\\Tester\\Testing\\1.0.0",
 		},
 	}
-
 	return expectations[runtime.GOOS]
+}
+
+// extractDomain extract an optional domain from a Windows username
+// e.g. "domain\\user" => "user"
+func extractDomain(s string) string {
+	p := strings.Split(s, "\\")
+	if len(p) > 1 {
+		return p[1]
+	}
+	return p[0]
 }
 
 func testApp() *App {
